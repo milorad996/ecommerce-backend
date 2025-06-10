8f47df99a6f7622dc45ec2b3826882871f37caf2
milorad996 <mips96.1@gmail.com>
2025-06-10 18:51:26 +0200

    index.php test

--- public/index.php
+++ public/index.php
@@ -1,5 +1,20 @@
 <?php
 
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    http_response_code(500);
+    echo json_encode(["error" => "Connection failed: " . $conn->connect_error]);
+    exit;
+}
+
+
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
