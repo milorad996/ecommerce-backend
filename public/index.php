c9402e2c0970cf51943b133debb288d819580a2e
milorad996 <mips96.1@gmail.com>
2025-06-10 19:05:31 +0200

    test index.php

--- public/index.php
+++ public/index.php
@@ -14,14 +14,19 @@ if ($conn->connect_error) {
     exit;
 }
 
-
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+    $r->get('/', function () {
+        header('Content-Type: application/json');
+        echo json_encode(["status" => "Backend is running"]);
+        return '';
+    });
+
     $r->post('/graphql', [App\Controller\GraphQL::class, 'handle']);
 });
 
@@ -32,15 +37,24 @@ $routeInfo = $dispatcher->dispatch(
 
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
-        // ... 404 Not Found
+        http_response_code(404);
+        echo json_encode(["error" => "Not Found"]);
         break;
+
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-        $allowedMethods = $routeInfo[1];
-        // ... 405 Method Not Allowed
+        http_response_code(405);
+        echo json_encode(["error" => "Method Not Allowed"]);
         break;
+
     case FastRoute\Dispatcher::FOUND:
         $handler = $routeInfo[1];
         $vars = $routeInfo[2];
-        echo $handler($vars);
+        if (is_array($handler)) {
+            $controller = new $handler[0];
+            $method = $handler[1];
+            echo $controller->$method($vars);
+        } else {
+            echo $handler($vars);
+        }
         break;
 }
